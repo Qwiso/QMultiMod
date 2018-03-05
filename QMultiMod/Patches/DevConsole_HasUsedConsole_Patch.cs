7b8bb382dec0dae7ce37c035683b86ae52f05252
Vlad-00003 <Vlad-00003@mail.ru>
2018-03-06 01:22:57 +0300

    New additions. Added option to allow players to get achievements after using console. Unlocked every steam item.
    Then the config is being created for the first time - it would be properly formatted.
    
    Patch now has error handler. =)

---
+++ QMultiMod/Patches/DevConsole_HasUsedConsole_Patch.cs
@@ -0,0 +1,16 @@
+﻿using Harmony;
+using System.Reflection;
+
+namespace QMultiMod.Patches
+{
+    [HarmonyPatch(typeof(DevConsole))]
+    [HarmonyPatch("HasUsedConsole")]
+    class DevConsole_HasUsedConsole_Patch
+    {
+        public static bool Prefix(ref bool __result)
+        {
+            __result = QMultiModSettings.Instance.AllowAchievements;
+            return false;
+        }
+    }
+}
---
+++ QMultiMod/Patches/Player_Awake_Patch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using Harmony;
+
+namespace QMultiMod.Patches
+{
+    [HarmonyPatch(typeof(Player))]
+    [HarmonyPatch("Awake")]
+    class Player_Awake_Patch
+    {
+        private static readonly MethodInfo UnlockItems =
+            typeof(SteamEconomy).GetMethod("UnlockItems", BindingFlags.Instance | BindingFlags.NonPublic);
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            var ret = codes[codes.Count - 1];
+            codes.Remove(ret);
+            codes.Add(new CodeInstruction(OpCodes.Ldarg_0));
+            codes.Add(new CodeInstruction(OpCodes.Call, UnlockItems));
+            codes.Add(ret);
+            return codes.AsEnumerable();
+        }
+    }
+}
