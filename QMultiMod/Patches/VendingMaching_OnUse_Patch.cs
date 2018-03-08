2df08baef07c03bd10d728d72dd90412183e19ff
Qwiso <>
2018-03-08 10:27:20 -0500

    checking VendingMachine.GetCanBeUsed before giving coffee

--- QMultiMod/Patches/VendingMaching_OnUse_Patch.cs
+++ QMultiMod/Patches/VendingMaching_OnUse_Patch.cs
@@ -1,7 +1,5 @@
 ﻿using Harmony;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Reflection.Emit;
 
 namespace QMultiMod.Patches
 {
@@ -9,33 +7,15 @@ namespace QMultiMod.Patches
     [HarmonyPatch("OnUse")]
     class VendingMaching_OnUse_Patch
     {
-        //public static void Postfix(VendingMachine __instance)
-        //{
-        //    if (QMultiModSettings.Instance.VendingMachineAlsoGivesCoffee)
-        //        CraftData.AddToInventory(TechType.Coffee, 1, false, false);
-        //}
-        private static readonly MethodInfo AddToInventory =
-            typeof(CraftData).GetMethod("AddToInventory",BindingFlags.Public | BindingFlags.Static);
-        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        private static readonly MethodInfo CanBeUsed = typeof(VendingMachine).GetMethod("GetCanBeUsed", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public static void Prefix(VendingMachine __instance)
         {
-            bool founded = false;
-            foreach (var instruction in instructions)
+            if (QMultiModSettings.Instance.VendingMachineAlsoGivesCoffee)
             {
-                if (founded)
-                {
-                    yield return new CodeInstruction(OpCodes.Ldc_I4,(int)TechType.Coffee);
-                    yield return new CodeInstruction(OpCodes.Ldc_I4_1);
-                    yield return new CodeInstruction(OpCodes.Ldc_I4_0);
-                    yield return new CodeInstruction(OpCodes.Ldc_I4_0);
-                    yield return new CodeInstruction(OpCodes.Call, AddToInventory);
-                    yield return new CodeInstruction(OpCodes.Pop);
-                    founded = false;
-                }
-                if (instruction.opcode.Equals(OpCodes.Pop))
-                {
-                    founded = true;
-                }
-                yield return instruction;
+                var canBeUsed = (bool)CanBeUsed.Invoke(__instance, null);
+                if (canBeUsed)
+                    CraftData.AddToInventory(TechType.Coffee, 1, false, false);
             }
         }
     }
