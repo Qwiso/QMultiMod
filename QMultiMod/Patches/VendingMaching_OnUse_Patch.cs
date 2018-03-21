0081ae5013237103dde0bc7e53bc30af14a909e4
Qwiso <>
2018-03-21 08:56:16 -0400

    one more coffee file

--- QMultiMod/Patches/VendingMaching_OnUse_Patch.cs
+++ QMultiMod/Patches/VendingMaching_OnUse_Patch.cs
@@ -1,22 +1,22 @@
-﻿using Harmony;
-using System.Reflection;
+﻿//using Harmony;
+//using System.Reflection;
 
-namespace QMultiMod.Patches
-{
-    [HarmonyPatch(typeof(VendingMachine))]
-    [HarmonyPatch("OnUse")]
-    class VendingMaching_OnUse_Patch
-    {
-        private static readonly MethodInfo CanBeUsed = typeof(VendingMachine).GetMethod("GetCanBeUsed", BindingFlags.NonPublic | BindingFlags.Instance);
+//namespace QMultiMod.Patches
+//{
+//    [HarmonyPatch(typeof(VendingMachine))]
+//    [HarmonyPatch("OnUse")]
+//    class VendingMaching_OnUse_Patch
+//    {
+//        private static readonly MethodInfo CanBeUsed = typeof(VendingMachine).GetMethod("GetCanBeUsed", BindingFlags.NonPublic | BindingFlags.Instance);
 
-        public static void Prefix(VendingMachine __instance)
-        {
-            if (QMultiModSettings.Instance.VendingMachineAlsoGivesCoffee)
-            {
-                var canBeUsed = (bool)CanBeUsed.Invoke(__instance, null);
-                if (canBeUsed)
-                    CraftData.AddToInventory(TechType.Coffee, 1, false, false);
-            }
-        }
-    }
-}
+//        public static void Prefix(VendingMachine __instance)
+//        {
+//            if (QMultiModSettings.Instance.VendingMachineAlsoGivesCoffee)
+//            {
+//                var canBeUsed = (bool)CanBeUsed.Invoke(__instance, null);
+//                if (canBeUsed)
+//                    CraftData.AddToInventory(TechType.Coffee, 1, false, false);
+//            }
+//        }
+//    }
+//}
